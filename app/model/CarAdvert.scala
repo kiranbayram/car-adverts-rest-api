5f9ef743a12c4d28b24c8d0bf19dc6dede84f09f
Bayram Kiran <kiranbayram@gmail.com>
2017-03-17 15:42:08 +0100

    Added first registration date to CarAdvert model.

--- app/model/CarAdvert.scala
+++ app/model/CarAdvert.scala
@@ -1,11 +1,13 @@
 package model
 
+import org.joda.time.LocalDate
+
 case class CarAdvert (
 	id: Long,
 	title: String,
 	fuelType: FuelTypes.Value,
 	price: Long,
 	isNew: Boolean,
-	mileage: Option[Long] = None//,
-	//firstRegistration: Option[LocalDate]
+	mileage: Option[Long] = None,
+	firstRegistration: Option[LocalDate] = None
 )
